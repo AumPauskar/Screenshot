03efa08a20e1c578a3263c5deb9748451f71539b
Aum <aumpauskar111@gmail.com>
2021-06-25 21:07:29 +0530

    Removed the need of log.dat text file and included a new format of screenshots

--- functions.py
+++ functions.py
@@ -5,33 +5,23 @@ import time
 # imporing all the modules required
 
 def Logs():
-    file = open('logs.dat', 'r')
-    data = file.read()
-    word = data.split('\n')
-    last_num = int(word[1])
-    last_num +=1
-    last = 'screenshot'+str(last_num)
-    file.close()
-    # logs is used to keep track of all the info hapened in the last
-    # taken screenshot
-    # the code written above are used to remember the last screenshot
+	# logs is used to keep track of all the info hapened in the last
+	# taken screenshot
+	# the code written above are used to remember the last screenshot
 
-    # now this part makes a new input in the .dat file
-    # firstly we use time
-    curtime = time.strftime('%I')+':'+time.strftime('%M')+':'+time.strftime('%S')+' '+time.strftime('%p')+'\n'+time.strftime('%d')+'/'+time.strftime('%m')+'/'+time.strftime('%y')
-    write_data = 'Screenshot\n'+str(last_num)+'\nLastScreenshot\n'+curtime
-    file = open('logs.dat', 'w')
-    file.write(write_data)
-    file.close()
-    
-    # this return function spits out the last screenshot number
-    return last
+	# now this part makes a new input in the .dat file
+	# firstly we use time
+	scntime = time.strftime('%Y')+time.strftime('%m')+time.strftime('%d')+'-'+time.strftime('%H')+time.strftime('%M')+time.strftime('%S')+'-'+str(time.perf_counter_ns())
+	write_scntime = 'Screenshot'+scntime
+	
+	# this return function spits out the last screenshot number
+	return write_scntime
 
 def Ts():
-    nameread = Logs()
-    final_name = nameread+'.png'
-    image = pgui.screenshot()
-    image = cv2.cvtColor(np.array(image),
-    cv2.COLOR_RGB2BGR)
+	nameread = Logs()
+	final_name = nameread+'.png'
+	image = pgui.screenshot()
+	image = cv2.cvtColor(np.array(image),
+	cv2.COLOR_RGB2BGR)
 
-    cv2.imwrite(final_name, image)
+	cv2.imwrite(final_name, image)
